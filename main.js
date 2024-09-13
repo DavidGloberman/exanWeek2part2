d64f92c3ed56aceb3197a1066b213313c04e73c8
02 <02@DESKTOP-A5DJ6R6>
2024-09-13 14:06:57 +0300

    Completion of the project (not during probation)

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 const editPage = document.getElementById("edit-page");
 const homePage = document.getElementById("home-page");
-const form = document.querySelector("#home-page > #form-wrapper");
+const createForm = document.querySelector("#home-page > #form-wrapper");
+const editForm = document.querySelector("#edit-page > #form-wrapper");
 const ascending = document.getElementById("ascending");
 const descending = document.getElementById("descending");
-const editForm = document.querySelector("#edit-page > #form-wrapper");
 
 const Soldiers = getLocalStorage() || [];
 
-const STATUS = {
-  ACTIVE: "Active",
-  RESERVE: "Reserve",
-  RETIRED: "Retired",
-};
-function generateId() {
-  return Math.random().toString(16).slice(2) + Date.now().toString(16);
+function getLocalStorage() {
+  return JSON.parse(localStorage.getItem("Soldiers"));
+}
+function setLocalStorage() {
+  localStorage.setItem("Soldiers", JSON.stringify(Soldiers));
+}
+
+function showHomePage() {
+  editPage.style.display = "none";
+  homePage.style.display = "block";
+}
+function showEditPage(soldier, index) {
+  homePage.style.display = "none";
+  editPage.style.display = "flex";
+  editForm["Full-name"].value = soldier.fullName;
+  editForm["rank"].value = soldier.rank;
+  editForm["position"].value = soldier.position;
+  editForm["platoon"].value = soldier.platoon;
+  editForm["mission-time"].value = soldier.missionTime;
+  editForm["status"].value = soldier.status;
+  editForm["cancel"].addEventListener("click", showHomePage, { once: true });
+  editForm.addEventListener(
+    "submit",
+    (e) => {
+      e.preventDefault();
+      editSoldier(e.target, index);
+      showHomePage();
+      setLocalStorage();
+      refreshTable();
+    },
+    { once: true }
+  );
+}
+
+function addSoldier(form) {
+  const newSoldier = createSoldier(form);
+  Soldiers.push(newSoldier);
+
+  setLocalStorage();
+  form.reset();
+  refreshTable();
 }
 function createSoldier(form) {
   const newSoldier = {
@@ -27,36 +61,23 @@ function createSoldier(form) {
   };
   return newSoldier;
 }
-
-function getLocalStorage() {
-  return JSON.parse(localStorage.getItem("Soldiers"));
+function generateId() {
+  return Math.random().toString(16).slice(2) + Date.now().toString(16);
+}
+function editSoldier(form, index) {
+  const newSoldier = {
+    ID: Soldiers[index].ID,
+    fullName: form["Full-name"].value,
+    rank: form["rank"].value,
+    position: form["position"].value,
+    platoon: form["platoon"].value,
+    status: form["status"].value,
+    missionTime: form["mission-time"].value,
+  };
+  Soldiers[index] = newSoldier;
+  form.reset(); //here a bag
 }
 
-function setLocalStorage() {
-  localStorage.setItem("Soldiers", JSON.stringify(Soldiers));
-}
-function showHomePage() {
-  editPage.style.display = "none";
-  homePage.style.display = "block";
-}
-function showEditPage(soldier) {
-  editPage.style.display = "flex";
-  homePage.style.display = "none";
-  editForm["Full-name"].value = soldier.fullName;
-  editForm["rank"].value = soldier.rank;
-  editForm["position"].value = soldier.position;
-  editForm["platoon"].value = soldier.platoon;
-  editForm["mission-time"].value = soldier.missionTime;
-  editForm["status"].value = soldier.status;
-}
-function addSoldier(form) {
-  const newSoldier = createSoldier(form);
-  Soldiers.push(newSoldier);
-
-  setLocalStorage();
-  form.reset();
-  refreshTable();
-}
 function refreshTable() {
   clearTable();
   buildTable();
@@ -74,6 +95,7 @@ function buildTable() {
     table.append(newRow);
   });
 }
+
 function createSoldierElements(soldier, index) {
   const newRow = document.createElement("tr");
 
@@ -90,11 +112,12 @@ function createSoldierElements(soldier, index) {
   status.innerText = soldier.status;
 
   const actions = createActions(soldier, index);
+  actions.classList.add("actions")
 
   newRow.append(fullName, rank, position, platoon, status, actions);
   return newRow;
 }
-function createActions(soldier, idx) {
+function createActions(soldier, index) {
   const newActions = document.createElement("td");
 
   const newButtonRemove = document.createElement("button");
@@ -102,22 +125,20 @@ function createActions(soldier, idx) {
   const newButtonEdit = document.createElement("button");
 
   newButtonRemove.textContent = "Remove";
-  newButtonMission.textContent = "Mission";
+  soldier.missionTime > 0
+    ? (newButtonMission.textContent = "Mission")
+    : (newButtonMission.textContent = "Mission Completed!");
   newButtonEdit.textContent = "Edit";
 
   setActionsEventListeners(
     { newButtonRemove, newButtonMission, newButtonEdit },
     soldier,
-    idx
+    index
   );
 
   newActions.append(newButtonRemove, newButtonMission, newButtonEdit);
   return newActions;
 }
-function deleteSoldier(idx) {
-  Soldiers.splice(idx, 1);
-}
-
 function setActionsEventListeners(actionButtons, soldier, idx) {
   actionButtons.newButtonRemove.addEventListener("click", () => {
     deleteSoldier(idx);
@@ -125,17 +146,28 @@ function setActionsEventListeners(actionButtons, soldier, idx) {
     refreshTable();
   });
   actionButtons.newButtonMission.addEventListener("click", () => {
-    // יש להוסיף לוגיקה שתפעיל את המשימה ואח''כ תעדכן במקומות הצריכים
-    // setLocalStorage();
-    // refreshTable();
-    // missionTime: 0,
-    console.log(
-      "יש להוסיף לוגיקה שתפעיל את המשימה ואח''כ תעדכן במקומות הצריכים"
-    );
+    startMission(actionButtons.newButtonMission, soldier);
   });
   actionButtons.newButtonEdit.addEventListener("click", () => {
-    showEditPage(soldier);
+    showEditPage(soldier, idx);
   });
+}
+function deleteSoldier(idx) {
+  Soldiers.splice(idx, 1);
+}
+function startMission(buttonMission, soldier) {
+  let timeToLeft = soldier.missionTime;
+  const intervalId = setInterval(() => {
+    if (timeToLeft <= 0) {
+      soldier.missionTime = 0;
+      setLocalStorage();
+      buttonMission.textContent = "Mission Completed!";
+      clearInterval(intervalId);
+      return;
+    }
+    buttonMission.textContent = timeToLeft;
+    timeToLeft--;
+  }, 1000);
 }
 
 function sortTableAsc() {
@@ -147,23 +179,11 @@ function sortTableDesc() {
   refreshTable();
 }
 
-form.addEventListener("submit", (e) => {
+createForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addSoldier(e.target);
 });
 
-editForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  editSoldier(e.target);
-});
-
-function editSoldier(form) {
-  console.log("כאן צריך לוגיקה שתעדכן את פרטי החייל");
-  showHomePage()
-  setLocalStorage();
-  form.reset();
-  refreshTable();
-}
 ascending.addEventListener("click", sortTableAsc);
 descending.addEventListener("click", sortTableDesc);
 
